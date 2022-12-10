d66f431182598e8f760167596293bb3a1bb02280
jhp phich <phich82@gmail.com>
2022-12-10 05:32:14 +0000

    update .gitpod.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,23 +10,25 @@ import config from 'orm.config';
 import { EventRepository } from './events/repositories/event.repository';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(
-    {
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 33060,
-      database: 'master_nestjs',
-      username: 'admin',
-      password: 'admin123',
-      entities: [Event],
-      // entities: ['src/events/entities/**/*.ts'],
-      synchronize: false, // false để khi bạn thay đổi trong entities nó sẽ không tự update DB
-      dropSchema: false,
-      migrations: [
-        // "src/database/migrations/*.ts",
-      ],
-    }
-  )],
+  imports: [
+    // TypeOrmModule.forRoot(
+    //   {
+    //     type: 'mysql',
+    //     host: '127.0.0.1',
+    //     port: 33060,
+    //     database: 'master_nestjs',
+    //     username: 'admin',
+    //     password: 'admin123',
+    //     entities: [Event],
+    //     // entities: ['src/events/entities/**/*.ts'],
+    //     synchronize: false, // false để khi bạn thay đổi trong entities nó sẽ không tự update DB
+    //     dropSchema: false,
+    //     migrations: [
+    //       // "src/database/migrations/*.ts",
+    //     ],
+    //   }
+    // )
+  ],
   controllers: [EventController, AppController],
   providers: [EventRepository, EventService, AppService, ApiResponse],
 })
